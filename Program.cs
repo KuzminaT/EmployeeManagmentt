fa35cfaf9588acf0bf4ee529a24441fe355669b7
Татьяна Кузьмина <tkuzmina344@gmail.com>
2026-04-29 19:03:08 +0300

    Добавили класс EmployeeForm

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace EmployeeManagment
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new EmployeeForm());
         }
     }
 }
